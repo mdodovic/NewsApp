91716244baf58e9036fd958507a018e164ce4363
mdodovic <matija.dodovic@gmail.com>
2022-11-28 20:40:08 +0100

    Add HttpLoggingInterseptor dependency

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "d156db305b5a4709aa3ab24294857ccc"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
