6be4b044048520d26dbb0c1a765df5816f2666eb
mdodovic <matija.dodovic@gmail.com>
2022-12-01 10:51:38 +0100

    View (Adapter, Model, Factory, Repository)

---
+++ app/src/main/java/com/example/newsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.newsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.R
+import com.example.newsapp.models.Article
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_prewiew,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(findViewById<ImageView>(R.id.ivArticleImage))
+
+            val tvSource = findViewById<TextView>(R.id.tvSource)
+            tvSource.text = article.source.name
+
+            val tvTitle = findViewById<TextView>(R.id.tvTitle)
+            tvTitle.text = article.title
+
+            val tvDescription = findViewById<TextView>(R.id.tvDescription)
+            tvDescription.text = article.description
+
+            val tvPublishedAt = findViewById<TextView>(R.id.tvPublishedAt)
+            tvPublishedAt.text = article.publishedAt
+
+            setOnItemClickListener {
+                onItemCLickListener?.let { it(article) }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemCLickListener : ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemCLickListener = listener
+    }
+
+}
--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -2,20 +2,33 @@ package com.example.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
+import com.example.newsapp.db.ArticleDatabase
+import com.example.newsapp.repository.NewsRepository
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val newsNavHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
+
 
     }
 }
