f821df247a7db90446a94372bf135d7037832ef4
mdodovic <matija.dodovic@gmail.com>
2022-12-02 15:41:16 +0100

    No internet connection check

--- app/src/main/java/com/example/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.AbsListView
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -62,6 +63,7 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
                     hideProgressBar(view)
                     response.message?.let { message ->
                         Log.e(TAG, "An error occured: $message")
+                        Toast.makeText(activity, "An error occured: $message", Toast.LENGTH_LONG).show()
                     }
                 }
                 is Resource.Loading -> {
--- app/src/main/java/com/example/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.AbsListView
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -84,6 +85,7 @@ class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
                     hideProgressBar(view)
                     response.message?.let { message ->
                         Log.e(TAG, "An error occured: $message")
+                        Toast.makeText(activity, "An error occured: $message", Toast.LENGTH_LONG).show()
                     }
                 }
                 is Resource.Loading -> {
